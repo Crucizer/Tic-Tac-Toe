9358ef1edae3e8c8e7b84093b1b364ddc6262401
Ayush Agarwal <ayushdiagarwal@gmail.com>
2020-01-20 18:26:16 +0530

    Just Changed Colours

--- tic.py
+++ tic.py
@@ -13,10 +13,12 @@ pg.display.set_caption('Tic Tac Toe')
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 # Clock Variables
 CLOCK = pg.time.Clock()
-FPS = 40
+FPS = 30
 
 # Board
 Matrix = [
@@ -34,6 +36,7 @@ class Board:
     winner = None
     tie = None
     # Running All The Functions
+
     def main(self):
         self.lines()
         self.print_thingy()
@@ -41,7 +44,7 @@ class Board:
         self.type_stuff()
         self.check_winner(Board.turn)
 
-    # Function just to draw lines 
+    # Function just to draw lines
     def draw_lines(self, x, y, width, height):
         pg.draw.rect(DISPLAY, BLACK, (x, y, width, height))
 
@@ -73,21 +76,28 @@ class Board:
                 n = Matrix[j][i]
                 xx = self.hori_gap * i + 0.33 * self.hori_gap
                 yy = self.ver_gap * j + 0.3 * self.ver_gap
-                self.print_stuff(100, str(n), BLACK, xx, yy)
+                if n == "X":
+                    self.print_stuff(100, str(n), GREEN, xx, yy)
+                else:
+                    self.print_stuff(100, str(n), BLUE, xx, yy)
 
     # When we click on the a block it would be selected
     def select_block(self):
         self.click = pg.mouse.get_pressed()
         self.mouse = pg.mouse.get_pos()
 
+        # As we know there are three blocks each consiting of three subblocks we're gonna get the vertical and   #horizontal box one by one and then combine them to get the subblock position
         if self.click[0] == 1:
+                # We're getting the which of the vertical box is being clicked through this
             ver_block = self.mouse[1] // (DISPLAY_HEIGHT // 3)
+            # We're getting the which of the horizontal box is being clicked through this
             hori_block = self.mouse[0] // (DISPLAY_WIDTH // 3)
             Board.key_i = ver_block
             Board.key_j = hori_block
 
     # Typing Os And Xs on the grid
     def type_stuff(self):
+        # Globaling the matrix list as we need to change it's values now
         global Matrix
         self.keys = pg.key.get_pressed()
         if Board.key_i != None and Board.key_j != None:
@@ -100,7 +110,7 @@ class Board:
                     Matrix[Board.key_i][Board.key_j] = "X"
                     Board.turn = "0"
             if self.keys[pg.K_SPACE]:
-                Matrix = [[""]*3, [""]*3, [""]*3]
+                Matrix = [[""] * 3, [""] * 3, [""] * 3]
                 Board.turn = "X"
                 Board.winner = None
                 Board.tie = None
@@ -108,31 +118,38 @@ class Board:
 
     # Checking For The Winner
     def check_winner(self, turn):
+        # Horizontally
         for i in Matrix:
             if i == ["X"] * 3:
                 Board.winner = "X"
             if i == ["O"] * 3:
                 Board.winner = "O"
+        # Vertically
         if Matrix[0][0] == Matrix[1][0] == Matrix[2][0] == "X"or Matrix[0][1] == Matrix[1][1] == Matrix[2][1] == "X"or Matrix[0][2] == Matrix[1][2] == Matrix[2][2] == "X":
             Board.winner = "X"
         if Matrix[0][0] == Matrix[1][0] == Matrix[2][0] == "O"or Matrix[0][1] == Matrix[1][1] == Matrix[2][1] == "O"or Matrix[0][2] == Matrix[1][2] == Matrix[2][2] == "O":
             Board.winner = "O"
-        if Matrix[0][0] == Matrix[1][1] == Matrix[2][2] == "X" or Matrix[0][2] ==  Matrix[1][1] ==  Matrix[2][0] == "X":
+        if Matrix[0][0] == Matrix[1][1] == Matrix[2][2] == "X" or Matrix[0][2] == Matrix[1][1] == Matrix[2][0] == "X":
             Board.winner = "X"
-        if Matrix[0][0] == Matrix[1][1] ==  Matrix[2][2] == "O" or Matrix[0][2] == Matrix[1][1] == Matrix[2][0] == "O":
+        # Diagonally
+        if Matrix[0][0] == Matrix[1][1] == Matrix[2][2] == "O" or Matrix[0][2] == Matrix[1][1] == Matrix[2][0] == "O":
             Board.winner = "O"
         if Board.winner != None:
-            self.print_stuff(110, f"Player {Board.winner}", RED, (DISPLAY_WIDTH//2)-150, (DISPLAY_WIDTH//2)-100)
-            self.print_stuff(110, "Won!", RED, (DISPLAY_WIDTH//2)-100, (DISPLAY_WIDTH//2)-30)
+            self.print_stuff(110, f"Player {Board.winner}", RED, (DISPLAY_WIDTH // 2) - 150, (DISPLAY_WIDTH // 2) - 100)
+            self.print_stuff(110, "Won!", RED, (DISPLAY_WIDTH //
+                                                2) - 100, (DISPLAY_WIDTH // 2) - 30)
 
         empty = []
         for i in Matrix:
             empty.append(all(i))
 
         if not Board.winner:
-            if empty == [True]*3:
-                self.print_stuff(110, "Tie!", RED, (DISPLAY_WIDTH//2)-70, (DISPLAY_WIDTH//2)-30)
+            if empty == [True] * 3:
+                self.print_stuff(
+                    110, "Tie!", RED, (DISPLAY_WIDTH // 2) - 70, (DISPLAY_WIDTH // 2) - 30)
 # The Main Function
+
+
 def main():
     while True:
         for event in pg.event.get():
